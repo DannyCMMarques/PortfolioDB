32c825acac36838d5f5be8711ecdca7402d63dd2
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-16 15:19:57 -0300

    feat: realização de remoção de arquivos do arquivo base

--- src/utils/enums/StatusSessao.ts
+++
@@ -1,7 +0,0 @@
-export const StatusSessao = {
-  NAO_INICIADA: "NAO_INICIADA",
-  EM_ANDAMENTO: "EM_ANDAMENTO",
-  FINALIZADA: "FINALIZADA",
-} as const;
-
-export type StatusSessao = typeof StatusSessao[keyof typeof StatusSessao];
