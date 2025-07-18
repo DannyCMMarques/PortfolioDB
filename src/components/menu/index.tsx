ada2448066bef2ecd961c20db893df95ffe445e6
Danielly Marques <danielly.marques@dbserver.com.br>
2025-07-18 15:46:27 -0300

    feat: correção de menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -5,35 +5,46 @@ import Logo from "./../../assets/logo_menu.png";
 export default function Menu() {
     return (
         <nav className="w-full px-4 sm:px-4 md:px-20 bg-[#85D4F4] text-[#414ABA] shadow-md  flex flex-row items-center justify-between">
-                <div className="flex items-center justify-between gap-4 sm:gap-4 md:gap-25">
-                    <a
-                        href="https://db.tec.br/"  target="_blank" rel="noopener noreferrer" 
-                        >
-                        <img
-                            src={Logo}
-                            alt="Logo"
-                            className="h-[60px] w-[60px] md:h-[87px] md:w-[87px] transition-all"
-                        />
-                    </a>
+            <div className="flex items-center justify-between gap-4 sm:gap-4 md:gap-25">
+                <a
+                    href="https://db.tec.br/" target="_blank" rel="noopener noreferrer"
+                >
+                    <img
+                        src={Logo}
+                        alt="Logo"
+                        className="h-[60px] w-[60px] md:h-[87px] md:w-[87px] transition-all"
+                    />
+                </a>
 
-                    <ul className="flex justify-between md:gap-25 text-center gap-4 sm:gap-4 flex-wrap">
-                        {menuContentItens.map((item) => (
-                            <li key={item.id}>
-                                <a href={item.link} target="_blank" rel="noopener noreferrer" className="hover:underline">
+                <ul className="flex justify-between md:gap-25 text-center gap-4 sm:gap-4 flex-wrap">
+                    {menuContentItens.map((item) => (
+                        <li key={item.id}>
+                            {item.label === "Currículo" ? (
+                                <a
+                                    href={item.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="hover:underline"
+                                >
                                     {item.label}
                                 </a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="flex justify-between gap-4 sm:gap-4 md:gap-10">
-                    <a href="https://github.com/DannyCMMarques" target="_blank" rel="noopener noreferrer">
-                        <img src={GithubIcon} alt="GitHub" className="w-6 h-6" />
-                    </a>
-                    <a href="https://br.linkedin.com/in/danny-marques" target="_blank" rel="noopener noreferrer">
-                        <img src={LinkedinIcon} alt="LinkedIn" className="w-6 h-6" />
-                    </a>
-                </div>
+                            ) : (
+                                <a href={item.link} className="hover:underline">
+                                    {item.label}
+                                </a>
+                            )}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div className="flex justify-between gap-4 sm:gap-4 md:gap-10">
+                <a href="https://github.com/DannyCMMarques" target="_blank" rel="noopener noreferrer">
+                    <img src={GithubIcon} alt="GitHub" className="w-6 h-6" />
+                </a>
+                <a href="https://br.linkedin.com/in/danny-marques" target="_blank" rel="noopener noreferrer">
+                    <img src={LinkedinIcon} alt="LinkedIn" className="w-6 h-6" />
+                </a>
+            </div>
         </nav>
     );
 }
